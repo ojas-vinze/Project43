ae389a8cf8960f4bae4b13efaa3744001d8956ce
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2021-04-20 14:37:27 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    // Question 1
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+
+    // Question 2
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N",700,150);
+    fill("lightblue");
+    text("Hint: Performs a particular task !!", 700,170);
+
+    // Question 3
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A",120,250);
+    fill("lightblue");
+    text("Hint: Stores all the data !!",120,270);
+}
